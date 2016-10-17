1dc90480cd54923f219b2d441309e17dd9667b8e
robertwangcf <363970611@qq.com>
2016-10-17 00:27:22 -0700

    Signed-off-by: robertwangcf <363970611@qq.com>

--- PriorityQueue/MeetingRooms.java
+++ PriorityQueue/MeetingRooms.java
@@ -1,4 +1,9 @@
-package javaapplication_test;
+ /** Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+* For example,
+* Given [[0, 30],[5, 10],[15, 20]],
+* return 2
+*/
+package javaapplication_test.PriorityQueue;
 import java.util.*;
 import java.lang.*;
 
