86537c6138299ea6c77fa937c4522f8ca5bb9958
robertwangcf <363970611@qq.com>
2016-10-23 23:57:54 -0700

    Signed-off-by: robertwangcf <363970611@qq.com>

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -1,1 +1,45 @@
 
+import java.util.LinkedList;
+
+/*
+ * Problem: Reverse a singly linked list.
+ */
+public class ReverseLinkedList {
+    static LinkedList list = new LinkedList();
+    //static String i = "1";
+   
+  
+   
+    public static void main(String[] args) {
+         Node head = new Node("1");
+         Node tail = new Node("2");
+         Node a = head;
+         Node b = a;
+         b = b.next;
+         try{
+   
+    list.add(head);
+    list.add(tail);
+     
+}
+    catch (Exception e) {
+    e.printStackTrace();
+    }
+         System.out.println(a.item);
+  
+}
+ private static class Node<E> {
+         E item;
+        Node<E> next;
+        Node<E> prev;
+   
+        Node(E i) {
+            this.item = i;
+        }
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
