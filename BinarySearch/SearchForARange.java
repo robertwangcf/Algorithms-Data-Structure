db0fabd9c2e449ff3f6a0bf900039a2fec0856d4
robertwangcf <363970611@qq.com>
2016-12-08 23:43:13 -0800

    Update SearchForARange.java

--- BinarySearch/SearchForARange.java
+++ BinarySearch/SearchForARange.java
@@ -1,5 +1,5 @@
-
-/*Given a sorted array of n integers, find the starting and ending position of a given target value.
+/*reference: https://leetcode.com/problems/search-for-a-range/
+Given a sorted array of n integers, find the starting and ending position of a given target value.
 * If the target is not found in the array, return [-1, -1].
 * Example
 * Given [5, 7, 7, 8, 8, 10] and target value 8,
