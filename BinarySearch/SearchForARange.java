bcff08df3b877a71bb5c03c50d694685f03b1027
robertwangcf <363970611@qq.com>
2016-12-10 00:19:30 -0800

    Update SearchForARange.java

--- BinarySearch/SearchForARange.java
+++ BinarySearch/SearchForARange.java
@@ -7,3 +7,51 @@ Given a sorted array of n integers, find the starting and ending position of a g
 
 */
 
+//solution, use two binary search, every search to find left / right bound. Time: O(2 * logN), Space: O(1)
+public class Solution {
+    public static int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int[] ret = new int[2];
+        int start = 0;
+        int end = nums.length - 1;
+        int left= start;
+        int right = end;
+        int middle;
+        //first find the left bound
+        while(left + 1 < right) {
+            middle = left + (right - left) / 2;
+            if (nums[middle] >= target) {
+                right = middle;
+            }
+            else {
+                left = middle;
+            }
+        }
+        ret[0] = nums[left] == target ? left :
+                 nums[right] == target ? right :
+                 -1;
+                 
+        //second binary search for the right bound
+            left = start;
+            right = end;
+          while(left + 1 < right) {
+            middle = left + (right - left) / 2;
+            if (nums[middle] > target) {
+                right = middle;
+            }
+            else {
+                left = middle;
+            }
+            }  
+            ret[1] = nums[right] == target ? right :
+                 nums[left] == target ? left :
+                 -1;
+            return ret;
+    }
+  public static void main(String[] args) {
+    int[] array = new int[]{5, 7, 7, 8, 8, 10};
+    int target = 8;
+    System.out.println(searchRange(array, target));
+}
