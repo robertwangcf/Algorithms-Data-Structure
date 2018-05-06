88c701e77f5de206716858a6b0580cc86f8f3464
robertwangcf <363970611@qq.com>
2018-05-05 19:03:45 -0700

    Refactor code and add mergeSort

--- src/BinarySearch/SearchForARange.java
+++ src/BinarySearch/SearchForARange.java
@@ -1,4 +1,4 @@
-/*reference: https://leetcode.com/problems/search-for-a-range/
+package BinarySearch;/*reference: https://leetcode.com/problems/search-for-a-range/
 Given a sorted array of n integers, find the starting and ending position of a given target value.
 * If the target is not found in the array, return [-1, -1].
 * Example
@@ -8,7 +8,7 @@ Given a sorted array of n integers, find the starting and ending position of a g
 */
 
 //solution, use two binary search, every search to find left / right bound. Time: O(2 * logN), Space: O(1)
-public class Solution {
+class Solution {
     public static int[] searchRange(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return new int[]{-1, -1};
@@ -16,42 +16,42 @@ public class Solution {
         int[] ret = new int[2];
         int start = 0;
         int end = nums.length - 1;
-        int left= start;
+        int left = start;
         int right = end;
         int middle;
         //first find the left bound
-        while(left + 1 < right) {
+        while (left + 1 < right) {
             middle = left + (right - left) / 2;
             if (nums[middle] >= target) {
                 right = middle;
-            }
-            else {
+            } else {
                 left = middle;
             }
         }
         ret[0] = nums[left] == target ? left :
-                 nums[right] == target ? right :
-                 -1;
-                 
+                nums[right] == target ? right :
+                        -1;
+
         //second binary search for the right bound
-            left = start;
-            right = end;
-          while(left + 1 < right) {
+        left = start;
+        right = end;
+        while (left + 1 < right) {
             middle = left + (right - left) / 2;
             if (nums[middle] > target) {
                 right = middle;
-            }
-            else {
+            } else {
                 left = middle;
             }
-            }  
-            ret[1] = nums[right] == target ? right :
-                 nums[left] == target ? left :
-                 -1;
-            return ret;
+        }
+        ret[1] = nums[right] == target ? right :
+                nums[left] == target ? left :
+                        -1;
+        return ret;
     }
-  public static void main(String[] args) {
-    int[] array = new int[]{5, 7, 7, 8, 8, 10};
-    int target = 8;
-    System.out.println(searchRange(array, target));
+
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 7, 7, 8, 8, 10};
+        int target = 8;
+        System.out.println(searchRange(array, target));
+    }
 }
---
+++ src/Sort/MergeSort.java
@@ -0,0 +1,57 @@
+package Sort;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String args[]) {
+        //for testing only
+        int test[] = {3,2,1,4,5};
+        sortIntegers2(test);
+        Arrays.stream(test)
+                .forEach(ele -> System.out.print(ele));
+    }
+    /**
+     * @param A: an integer array
+     * @return: nothing
+     */
+    public static void sortIntegers2(int[] A) {
+        // write your code here
+        if (A == null || A.length == 0) {
+            return;
+        }
+        int[] helper = new int[A.length];
+        int left = 0;
+        int right = A.length - 1;
+        mergeSort(A, left, right, helper);
+    }
+
+    private static void mergeSort(int[] A, int left, int right, int[] helper) {
+        if (left < right) {
+            int middle = left + (right - left) / 2;
+            mergeSort(A, left, middle, helper);
+            mergeSort(A, middle + 1, right, helper);
+            mergeTwoSortedArray(A, left, middle, right, helper);
+        }
+    }
+
+    private static void mergeTwoSortedArray(int[] A, int left, int middle, int right, int[] helper) {
+        for (int i = left; i <= right; i++) {
+            helper[i] = A[i];
+        }
+        int startPointer1 = left;
+        int startPointer2 = middle + 1;
+        int i = left;
+        while (startPointer1 < (middle + 1) && startPointer2 < (right + 1)) {
+            if (helper[startPointer1] <= helper[startPointer2]) {
+                A[i++] = helper[startPointer1++];
+            } else {
+                A[i++] = helper[startPointer2++];
+            }
+        }
+
+        int remainingLeftPart = middle - startPointer1;
+        while(remainingLeftPart-- >= 0) {
+            A[i++] = helper[startPointer1++];
+        }
+    }
+}
